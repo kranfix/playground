beb580a94473d084e14c01cacf7285a050371d2b
Frank Moreno <frankmoreno1993@gmail.com>
2014-11-17 21:35:50 -0500

    MPU-Calib: maf.h updated

--- mpucalib/maf.h
+++ mpucalib/maf.h
@@ -1,10 +1,16 @@
 #ifndef MAF_H
 #define MAF_H
 
+template<type T>
 class maf
 {
 public:
-    maf();
+  maf(int len) : n(len) ;
+  T mean();
+private:
+  int n; // length
+  int p; // pivot
+  T * V;
 };
 
 #endif // MAF_H
